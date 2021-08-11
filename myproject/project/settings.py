4bbe3d6dc8f8693690d9f09a343fd770e0a658cd
tu-akyas <63646998+tu-akyas@users.noreply.github.com>
2021-08-11 15:26:21 +0530

    Update settings.py

--- myproject/project/settings.py
+++ myproject/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,6 @@ EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/'
 
 #Static Root
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
